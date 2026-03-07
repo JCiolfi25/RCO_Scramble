5a50ba2e1888d0d69a2457b16efadb984ba8cd08
JCiolfi2 <ciolfij@gmail.com>
2026-03-06 21:28:13 -0500

    integer value getting passed in and used!!

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def hello():
     return 'Hello, World!'
 
 @app.route('/hello')
+#http://127.0.0.1:5000/hello?name=John
 def hello_name():
     name=request.args.get("name","Flask")
     return f"Hello, {escape(name)}!"
+
+
+@app.route('/int/<int:num>')
+#http://127.0.0.1:5000/int/55
+def do_math(num):
+    return f"The square of {num} is {num**2}!"
