4f9f60be93e9fe2f3dc789b8574ce4219bc8a1d7
JCiolfi <ciolfij@gmail.com>
2026-05-24 11:38:20 -0400

    updated names

--- GraphTheory.py
+++ GraphTheory.py
@@ -80,16 +80,16 @@ class Round:
         for team in self.teams:
             # team.Print()
             pass
-        if print_edges:
+        if print_edges: # only print 100 bc console limitations
             print("Edges:")
             self.edges.sort(key=lambda e: e.weight)
-            for edge in self.edges:#.sort(key=lambda e: e.weight):
+            for edge in self.edges[:100]: # only print the first 100 edges
                 print(f"  {edge.team1.name} - {edge.team2.name}: {edge.weight}")
     def WeightEdges(self, algo_params):
         for edge in self.edges:
             edge.WeightSelf(algo_params)
         self.isWeighted = True
-    def Cull(self, numCourts=None): # Select games with lowest weights; if numCourts is specified, select that many games; otherwise, select as many games as possible without repeating teams
+    def Cull(self, numCourts=None, printEdges=False): # Select games with lowest weights; if numCourts is specified, select that many games; otherwise, select as many games as possible without repeating teams
         # Round.Cull() returns the list of games to be played this round, based on edge weights
         self.edges.sort(key=lambda e: e.weight)
         selected_games = list()
@@ -101,6 +101,10 @@ class Round:
                 if len(selected_games) == numCourts: # if numCourts is unspecified and therefore None, this condition will never be true and it will select as many games as possible without repeating teams; if numCourts is specified, it will select that many games
                     break
         self.isCulled = True
+        if printEdges:
+            print("Selected Games:")
+            for game in selected_games:
+                print(f"  {game.team1.name} vs {game.team2.name}")
         return selected_games
 class Edge:
     def __init__(self, team1, team2):
@@ -384,8 +388,11 @@ def GenerateSchedule(all_teams_list, algo_params, num_rounds_sched, num_courts=N
     games_added = 0
     for i in range(num_rounds_sched):
         master_round.WeightEdges(algo_params=algo_params) #re-weight the edges between rounds
-        # if i+1 ==5: master_round.Print(print_edges=True)
-        selected_games = master_round.Cull(numCourts=num_courts) #num_courts is the simultaneous number of games to be played
+        if i+1 ==6: 
+            master_round.Print(print_edges=True) #DEBUG print the edges 
+            selected_games = master_round.Cull(numCourts=num_courts, printEdges=True) #num_courts is the simultaneous number of games to be played
+        else:
+            selected_games = master_round.Cull(numCourts=num_courts) #num_courts is the simultaneous number of games to be played
         for game in selected_games: 
             if num_courts == None or (games_added < num_courts * num_rounds_sched): # Only add up to num_courts * num_rounds games to the schedule, since that's the maximum that can be played in the given number of rounds and courts; if num_courts is None, this condition will never be true and it will add all games
                 if verbose:
@@ -467,6 +474,6 @@ def SweepTest():
 if __name__ == "__main__":
     algo_params = AlgoParams(repeat_exponential=2, opponent_history_weight=1, teammate_history_weight=5, games_played_weight=100, recent_rounds_weight=000.0001) # This appears to be the best combo
     # Main(algo_params=algo_params, num_rounds=12, num_courts=1, num_men=3, save_csvs=True, print_overall=True, print_individuals=True)
-    Main(names_men=['John', 'Bob'], names_women=['Alice', 'Jane'], algo_params=algo_params, num_rounds=12, num_courts=1, save_csvs=True, print_overall=True, print_individuals=True)
+    Main(names_men=['Jake', 'Tommy', 'Tor', 'Wes', 'Matt', 'Rupak', 'Markiesh', 'Ray'], names_women=['Meghan', 'Kiley', 'Lindy','Sarah', 'Tiffani', 'Sacha', 'Amanda', 'Mary'], algo_params=algo_params, num_rounds=8, num_courts=4, save_csvs=True, print_overall=True, print_individuals=False)
     # SweepTest()
     print("Done")
